0d241965b3edd9f25a931daf0bf4b0e2fbf5d18f
James Cho <chojames500@gmail.com>
2021-03-24 03:07:56 +0900

    AddAlbum view's UI added

---
+++ YukikaHub/YukikaHub.UI/Dependency Properties/LabeledTextBox.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace YukikaHub.UI.Dependency_Properties
+{
+    public class LabeledTextBox : DependencyObject
+    {
+        #region Text Property
+        public static string GetText(DependencyObject obj)
+        {
+            return (string)obj.GetValue(TextProperty);
+        }
+
+        public static void SetText(DependencyObject obj, string value)
+        {
+            obj.SetValue(TextProperty, value);
+        }
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register(
+                "Text",
+                typeof(string),
+                typeof(LabeledTextBox),
+                new PropertyMetadata(""));
+        #endregion
+
+        #region Style Property
+        public static Style GetStyle(DependencyObject obj)
+        {
+            return (Style)obj.GetValue(StyleProperty);
+        }
+
+        public static void SetStyle(DependencyObject obj, Style value)
+        {
+            obj.SetValue(StyleProperty, value);
+        }
+
+        public static readonly DependencyProperty StyleProperty =
+            DependencyProperty.Register(
+                "Style",
+                typeof(Style),
+                typeof(LabeledTextBox));
+        #endregion
+
+    }
+}
