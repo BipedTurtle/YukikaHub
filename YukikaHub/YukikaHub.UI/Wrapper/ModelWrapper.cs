7f7729cb1a4962d67ab6e3ff8c2d50350995c000
James Cho <chojames500@gmail.com>
2021-03-26 05:05:19 +0900

    NotifyDataErrorInfo and ModelWrapper class added

---
+++ YukikaHub/YukikaHub.UI/Wrapper/ModelWrapper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YukikaHub.UI.Wrapper
+{
+    public class ModelWrapper<T> : NotifyDataErrorInfoBase
+    {
+        public T Model { get; }
+        public ModelWrapper(T model)
+        {
+            this.Model = model;
+        }
+
+        public TValue GetValue<TValue>([CallerMemberName] string propertyName = null)
+        {
+            return (TValue)typeof(T).GetProperty(propertyName).GetValue(this.Model);
+        }
+
+        public void SetValue<TValue>(TValue value, [CallerMemberName] string propertyName = null)
+        {
+            typeof(T).GetProperty(propertyName).SetValue(this.Model, value);
+            OnPropertyChanged(propertyName);
+            this.ValidateInternal(propertyName, value);
+        }
+
+        private void ValidateInternal<TValue>(string propertyName, TValue value)
+        {
+            base.ClearErorrs(propertyName);
+
+            this.ValidateDataAnnotations(propertyName, value);
+            this.ValidateCustom(propertyName);
+        }
+
+        private void ValidateDataAnnotations(string propertyName, object value)
+        {
+            var results = new List<ValidationResult>();
+            var context = new ValidationContext(Model) { MemberName = propertyName };
+            Validator.TryValidateProperty(value, context, results);
+
+            foreach (var result in results)
+                base.AddError(propertyName, result.ErrorMessage);
+        }
+
+        private void ValidateCustom(string propertyName)
+        {
+            var errors = this.ValidateProperty(propertyName);
+            if (errors == null)
+                return;
+
+            foreach (var error in errors)
+                base.AddError(propertyName, error);
+        }
+
+        protected virtual IEnumerable<string> ValidateProperty(string propertyName)
+        {
+            return null;
+        }
+    }
+}
---
+++ YukikaHub/YukikaHub.UI/Wrapper/NotifyDataErrorInfoBase.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using YukikaHub.UI.ViewModels;
+
+namespace YukikaHub.UI.Wrapper
+{
+    public class NotifyDataErrorInfoBase : ViewModelBase, INotifyDataErrorInfo
+    {
+        private Dictionary<string, List<string>> _errorsByPropertyName = new Dictionary<string, List<string>>();
+
+        public bool HasErrors => _errorsByPropertyName.Any();
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            return _errorsByPropertyName.ContainsKey(propertyName) ?
+                        _errorsByPropertyName[propertyName] :
+                        null;
+        }
+
+        protected virtual void OnErrorsChanged(string propertyName)
+        {
+            this.ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+            base.OnPropertyChanged(nameof(HasErrors));
+        }
+
+        public void AddError(string propertyName, string error)
+        {
+            if (!_errorsByPropertyName.ContainsKey(propertyName))
+                _errorsByPropertyName[propertyName] = new List<string>();
+
+            var errorsByProperty = _errorsByPropertyName[propertyName];
+            if (!errorsByProperty.Contains(error)) {
+                errorsByProperty.Add(error);
+                OnErrorsChanged(propertyName);
+            }
+        }
+
+        public void ClearErorrs(string propertyName)
+        {
+            if (_errorsByPropertyName.ContainsKey(propertyName))
+                _errorsByPropertyName.Remove(propertyName);
+            OnErrorsChanged(propertyName);
+        }
+    }
+}
