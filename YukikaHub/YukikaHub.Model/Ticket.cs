ebcafe9e6a21f5ac65934846233bf3cd7b87b3fa
James Cho <chojames500@gmail.com>
2021-04-04 19:07:04 +0900

    Ticket Entity  Upated
    Date prop added

--- YukikaHub/YukikaHub.Model/Ticket.cs
+++ YukikaHub/YukikaHub.Model/Ticket.cs
@@ -14,6 +14,7 @@ namespace YukikaHub.Model
         [Key]
         public int Id { get; set; }
         public string Title { get; set; }
+        public DateTime Date { get; set; }
         public double Price { get; set; }
         public byte[] Image { get; set; }
         public string Description { get; set; }
