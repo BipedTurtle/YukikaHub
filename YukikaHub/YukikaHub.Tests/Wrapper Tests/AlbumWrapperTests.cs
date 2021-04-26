c2a71307347705495ad9f87b334534ea6dc8d7e2
James Cho <chojames500@gmail.com>
2021-04-26 16:07:25 +0900

    Album and Ticket wrapper tests added

---
+++ YukikaHub/YukikaHub.Tests/Wrapper Tests/AlbumWrapperTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using YukikaHub.Model;
+using YukikaHub.UI.Wrapper;
+using Moq.Protected;
+using Moq;
+using System.Collections;
+
+namespace YukikaHub.Tests.Wrapper_Tests
+{
+    public class AlbumWrapperTests
+    {
+        private AlbumWrapper _albumWrapper;
+
+        public AlbumWrapperTests()
+        {
+            _albumWrapper = new AlbumWrapper(new Album());
+        }
+
+        [Theory]
+        [MemberData(nameof(GetInvalidDates))]
+        public void AlbumReleasedBeforeKikasBirthDate_ShouldYieldValidationError(DateTime released)
+        {
+            _albumWrapper.Released = released;
+            var errors = _albumWrapper.GetErrors(nameof(_albumWrapper.Released));
+
+            int count = 0;
+            foreach (var error in errors) {
+                count++;
+                break;
+            }
+
+            Assert.True(count > 0);
+        }
+
+        public static IEnumerable<object[]> GetInvalidDates()
+        {
+            yield return new object[] { new DateTime(1993, 2, 15) };
+            yield return new object[] { new DateTime(1800, 1, 1) };
+            yield return new object[] { new DateTime(1991, 3, 3) };
+        }
+    }
+}
---
+++ YukikaHub/YukikaHub.Tests/Wrapper Tests/TicketWrapperTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YukikaHub.Model;
+using YukikaHub.UI.Wrapper;
+using Xunit;
+using Moq;
+
+namespace YukikaHub.Tests.Wrapper_Tests
+{
+    public class TicketWrapperTests
+    {
+        private TicketWrapper _ticketWrapper;
+
+        public TicketWrapperTests()
+        {
+            _ticketWrapper = new TicketWrapper(new Ticket());
+        }
+
+        [Theory]
+        [MemberData(nameof(GetDatesBeforeToday))]
+        public void TicketShouldNotBeBeforeToday(DateTime date)
+        {
+            _ticketWrapper.Date = date;
+            var errors = _ticketWrapper.GetErrors(nameof(_ticketWrapper.Date));
+            var enumerator = errors.GetEnumerator();
+
+            enumerator.MoveNext();
+            Assert.NotNull(enumerator.Current);
+        }
+
+
+        public static IEnumerable<object[]> GetDatesBeforeToday()
+        {
+            yield return new object[] { DateTime.Today.AddMinutes(-1)};
+            yield return new object[] { DateTime.Today.AddDays(-22) };
+            yield return new object[] { DateTime.Today.AddMonths(-1) };
+        }
+    }
+}
